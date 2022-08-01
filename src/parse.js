819828ae8d9a657c297f80522e56a92a948df646
morsa <voxfoot@gmail.com>
2022-08-02 00:01:26 +0500

    parse

--- src/parse.js
+++ src/parse.js
@@ -2,6 +2,5 @@ export default (res) => {
   const { contents } = res.data;
   const parser = new DOMParser();
   const HTMLdocument = parser.parseFromString(contents, 'text/xml');
-  console.log(HTMLdocument);
   return HTMLdocument;
 };
